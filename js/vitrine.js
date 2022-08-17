0b439196064ca4eed3f94bc000bbc634fa361d72
Bruno A <bruno_arruda_ba@hotmail.com>
2022-08-17 18:02:57 -0300

    Update
    Co-authored-by: Carlos Zillig <CarlosZillig@users.noreply.github.com>

--- js/vitrine.js
+++ js/vitrine.js
@@ -1,51 +1,54 @@
+//list
 const productList = document.querySelector(".list");
 
-const produtos = [{
-        id: 0,
-        title: "Vestido Roxo",
-        img: "/assets/img/product1.jpg",
-        quantidade: 0
-    },
-    {
-        id: 1,
-        title: "Vestido Cinza",
-        img: "/assets/img/product2.jpg",
-        quantidade: 0
-    }
-]
+const produtos = [
+   {
+      id: 0,
+      title: "Vestido Roxo",
+      img: "/assets/img/product1.jpg",
+      quantidade: 0,
+   },
+   {
+      id: 1,
+      title: "Vestido Cinza",
+      img: "/assets/img/product2.jpg",
+      quantidade: 0,
+   }
+];
 
 productAdd = () => {
-    produtos.forEach((item) => {
-        productList.innerHTML += `
+   produtos.forEach((item) => {
+      productList.innerHTML += `
         <div class="product-single">
-            <img class="product__banner product__banner--4" src="`+item.img+`"/>
-            <p class="product__title">`+item.title+`</p>
+            <img class="product__banner product__banner--4" src="`+item.img +`"/>
+            <p class="product__title">` + item.title +`</p>
             <a key="`+item.id+`" href="#" class="btn">Adicionar ao carrinho</a>
         </div>
-        `  
-    })
-}
+        `;
+   });
+};
 
 productAdd();
 
+//CARRINHO
 var btnCarrinho = document.querySelectorAll(".btn");
-for(let i = 0; i < produtos.length; i++) {
-    btnCarrinho[i].addEventListener("click", (event) => {
-        event.preventDefault();
-        let key = btnCarrinho[i].getAttribute('key');
-        produtos[key].quantidade++;
-        atualizarCarrinho();
-    })
+for (let i = 0; i < produtos.length; i++) {
+   btnCarrinho[i].addEventListener("click", (event) => {
+      event.preventDefault();
+      let key = btnCarrinho[i].getAttribute("key");
+      produtos[key].quantidade++;
+      atualizarCarrinho();
+   });
 }
 
 function atualizarCarrinho() {
-    var containerCarrinho = document.querySelector(".carrinho")
-    containerCarrinho.innerHTML = "";
-    produtos.forEach((item) => {
-        containerCarrinho.innerHTML += `
-            <p>`+item.title+`</p> <br>
-            <p>`+item.quantidade+`</p>
-        `  
-    })
-}
-
+   var containerCarrinho = document.querySelector(".carrinho");
+   containerCarrinho.innerHTML = "";
+   produtos.forEach((item) => {
+      containerCarrinho.innerHTML +=
+         `
+         <p>`+item.title +`</p> <br>
+         <p>`+item.quantidade +`</p>
+        `
+   });
+};
